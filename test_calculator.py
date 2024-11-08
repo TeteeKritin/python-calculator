95775e6b1be3dcfa62a0173df8ab8ce40030cf60
Kritin Chitchobtham <74193071+TeteeKritin@users.noreply.github.com>
2024-11-08 04:51:55 +0000

    Added test and fix all error

--- test_calculator.py
+++ test_calculator.py
@@ -9,7 +9,34 @@ class TestCalculator(unittest.TestCase):
     def test_add(self):
         self.assertEqual(self.calc.add(1, 2), 3)
 
+    def test_add2(self):
+        self.assertEqual(self.calc.add(3,4), 7)
+
+    def test_sub(self):
+        self.assertEqual(self.calc.subtract(4,3),1)
+
+    def test_sub2(self):
+        self.assertEqual(self.calc.subtract(5,2),3)
+
+    def test_mul(self):
+        self.assertEqual(self.calc.multiply(10,0), 0)
+
+    def test_mul2(self):
+        self.assertEqual(self.calc.multiply(5,19), 95)
+
+    def test_div(self):
+        self.assertEqual(self.calc.divide(10, 2), 5)
+
+    def test_div2(self):
+        self.assertEqual(self.calc.divide(6, 2), 3)
+
+    def test_mod(self):
+        self.assertEqual(self.calc.modulo(10, 2), 0)
+    
+    def test_mod2(self):
+        self.assertEqual(self.calc.modulo(5,2), 1)
+
     # Add the following test methods to the TestCalculator class:
 
 if __name__ == '__main__':
-    unittest.main()
+    unittest.main(verbosity=2)
